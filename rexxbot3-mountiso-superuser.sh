77921e3b850e8c71d2f7fe91f9b1ec5a65a7bd14
Bastian Bittorf <bb@npl.de>
2021-06-20 18:26:39 +0200

    mounter: cleanup + autoloop

--- rexxbot3-mountiso-superuser.sh
+++ rexxbot3-mountiso-superuser.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 #
-# this runs as 'root' and awaits commands from 'rexxbot3.sh' in 'mountjob.txt'
-# we sanitize the input and run it regulary, e.g.:
+# this runs as 'root' and awaits commands from 'rexxbot3.sh' in
+# file 'rexxbox-mountjob.txt' - we sanitize the input and run it regulary
 #
-# while :; do ./rexxbot3-mountiso-superuser.sh; sleep 1; done
+# TODO!
+# apt-get install inotify-tools
+
+log()
+{
+	>&2 printf '%s\n' "$1"
+}
 
 mktemp_get_prefix()
 {
@@ -11,44 +17,51 @@ mktemp_get_prefix()
 	mktemp -u | cut -d'.' -f1
 }
 
+test "$( id -u )" -eq 0 || {
+	log "[ERROR] must run as superuser"
+	exit 1
+}
+
 alias explode='set -f;set +f --'
 
-# directory-name build from 'unbox_container-mode' - see scan_dir() in file 'rexxbot3.sh'
-for JOBFILE in "$( mktemp_get_prefix )"*/mountjob.txt; do {
-	read -r LINE 2>/dev/null <"$JOBFILE" || continue
-	case "$( id -u )" in 0) ;; *) logger -s "[ERROR] must run as superuser"; exit 1 ;; esac
+while true; do {
+	sleep 1
+	# directory-name build from 'unbox_container-mode' - see scan_dir() in file 'rexxbot3.sh'
+	for JOBFILE in "$( mktemp_get_prefix )"*/rexxbot-mountjob.txt; do {
+		read -r LINE 2>/dev/null <"$JOBFILE" || continue
 
-	# shellcheck disable=SC2086
-	explode $LINE
+		# shellcheck disable=SC2086
+		explode $LINE
 
-	# e.g. mount   /my/file.iso /this/dir iso9660
-	# e.g. unmount /my/file.iso /this/dir
-	ACTION="$1"		# mount|unmount|done
-	CONTAINER="$2"		# filename
-	MOUNTDIR="$3"		# dirname
-	FSTYPE="${4:-auto}"	# https://askubuntu.com/questions/143718/mount-you-must-specify-the-filesystem-type
-				# cat /proc/filesystems
+		# e.g. mount   /my/file.iso /this/dir iso9660
+		# e.g. unmount /my/file.iso /this/dir
+		ACTION="$1"		# mount|unmount|done
+		CONTAINER="$2"		# filename
+		MOUNTDIR="$3"		# dirname
+		FSTYPE="${4:-auto}"	# https://askubuntu.com/questions/143718/mount-you-must-specify-the-filesystem-type
 
-	[ "$ACTION" = 'done' ] || {
-		test -f "$CONTAINER" || ACTION='fail'
-		test -d "$MOUNTDIR"  || ACTION='fail'
-	}
+		[ "$ACTION" = 'done' ] || {
+			log "file: $JOBFILE | line: $LINE"
+			test -f "$CONTAINER" || ACTION='fail'
+			test -d "$MOUNTDIR"  || ACTION='fail'
+		}
 
-	case "$ACTION" in
-		'done')
-		;;
-		'mount')
-			echo 'done' >"$JOBFILE"
-			grep -q "$FSTYPE"$ /proc/filesystems && \
-				mount -t "$FSTYPE" -o loop,user "$CONTAINER" "$MOUNTDIR" 2>/dev/null
-		;;
-		'unmount')
-			echo 'done' >"$JOBFILE"
-			mount | grep -q ^"$CONTAINER on $MOUNTDIR type" && \
-				umount "$MOUNTDIR"
-		;;
-		*)
-			logger -s "ACTION: $ACTION LINE: $LINE"
-		;;
-	esac
+		case "$ACTION" in
+			'done')
+			;;
+			'mount')
+				echo 'done' >"$JOBFILE"
+				grep -q "$FSTYPE"$ /proc/filesystems && \
+					mount -t "$FSTYPE" -o loop,user "$CONTAINER" "$MOUNTDIR" 2>/dev/null
+			;;
+			'unmount')
+				echo 'done' >"$JOBFILE"
+				mount | grep -q ^"$CONTAINER on $MOUNTDIR type" && \
+					umount "$MOUNTDIR"
+			;;
+			*)
+				log "ACTION: $ACTION LINE: $LINE"
+			;;
+		esac
+	} done
 } done
